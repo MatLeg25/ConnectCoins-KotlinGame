35a7a78c95c70306df6fe68d88cc53360403b964
MatLeg <mat.legierskii@gmail.com>
2023-08-21 19:49:53 +0200

    generate table, on cell click change background color

--- app/src/main/java/com/example/connectcoins/MainActivity.kt
+++ app/src/main/java/com/example/connectcoins/MainActivity.kt
@@ -5,23 +5,16 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -39,14 +32,15 @@ import com.example.connectcoins.ui.theme.ConnectCoinsTheme
 import kotlin.random.Random
 
 //todo move to cofnig class
-val SIZE = 3
-val RANGE = 0..SIZE
+val SIZE = 24
+val RANGE = 0 until SIZE
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
             ConnectCoinsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -54,83 +48,48 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 
+                    val data: List<Cell> = RANGE.map { item ->
+                        Cell(item, "Cell$item", mutableStateOf(false))
+                    }
 
-//                    val data = RANGE.map { item ->
-//                        Cell(item, "Cell$item", mutableStateOf(false))
-//                    }
                     val dataHeader = RANGE.map { item ->
-                        Cell(item, "Column $item", false)
+                        Cell(item, "Column $item", mutableStateOf(false))
                     }
-
-//todo bulid array
-                    val row: Array<Cell> = RANGE.map {
-                           Cell(it, "Column $it", false)
-                        }.toTypedArray()
-
-                    val arrayData : Array<Array<Cell>> = RANGE.map {
-                        row
-                    }.toTypedArray()
-
-
-                    var c = 0
-                    for (x in RANGE) {
-                        for (y in RANGE) {
-                            Log.w("elox",">>>[${x}[${y}]=$c")
-                            arrayData[x][y] = Cell(c++, "Cell $c", false)
-                        }
-                    }
-
-                    var display = ""
-                    for (x in RANGE) {
-                        for (y in RANGE) {
-                            display+="${arrayData[x][y].index} |"
-                        }
-                        display+="\n"
-                    }
-                    Log.e("elox","DISPLAY: \n $display")
 
                     val x = Random.nextBoolean()
 
-                    TableScreen(SIZE, arrayData, dataHeader, x)
+                    TableScreen(SIZE, data, dataHeader, x)
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun RowScope.TableCell(
-    text: String,
-    weight: Float,
-    selected: Boolean,
-    onClick: () -> Unit
+class Cell(
+    val index: Int,
+    var text: String,
+    var state: MutableState<Boolean>
 ) {
-    Surface(
-        modifier = Modifier
-            .border(1.dp, Color.Black)
-            .weight(weight),
-        color = if(selected) Color.Red else Color.Transparent,
-        onClick = onClick
-    ) {
-        Text(
-            text = text.toString(),
-        )
+    fun changeState() {
+        state.value = !state.value
     }
 }
 
-class Cell(
-    val index: Int,
-    val text: String,
-    //var state: MutableState<Boolean>
-    var state: Boolean
-) {
-    fun changeState() {
-        Log.w("elox","Cell clicked! Text=$text, Index: $index")
-        Log.w("elox","State0: $state")
-        state = !state
-        Log.w("elox","State1: $state")
+@Composable
+fun CellItem(item: Cell) {
+    Box(
+        modifier = Modifier
+            .padding(8.dp)
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(5.dp))
+            .background(if (item.state.value) Color.Red else Color.Green)
+            .clickable {
+                item.changeState()
+            },
 
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(text = item.text)
     }
 }
 
@@ -138,49 +97,23 @@ class Cell(
 @Composable
 fun TableScreen(
     SIZE: Int = 5,
-    data: Array<Array<Cell>> = arrayOf(
-        arrayOf(Cell(1, "Cell", false)),
-        arrayOf(Cell(1, "Cell", false)),
-    ),
+    data: List<Cell> = RANGE.map { item ->
+        Cell(item, "Cell$item", mutableStateOf(false))
+    },
     dataHeader: List<Cell> = RANGE.map { item ->
-        Cell(item, "Cell$item", false)
+        Cell(item, "Cell$item", mutableStateOf(false))
     },
     state: Boolean = false
 ) {
 
-    val x = remember { mutableStateOf(state) }
-   // val data = remember { mutableStateOf(data22) }
-
-    // Each cell of a column must have the same weight.
-    val column1Weight = .3f // 30%
-    val column2Weight = .7f // 70%
-    val state = rememberLazyGridState(
-        initialFirstVisibleItemIndex = 99
-    )
     LazyVerticalGrid(
-        columns = GridCells.Fixed(SIZE), //Adaptive 100dp
-        state = state,
+        columns = GridCells.Fixed(4), //Adaptive 100dp
+        //state = state,
         content = {
-                  items(100){ i ->
-                      Box(
-                          modifier = Modifier
-                              .padding(8.dp)
-                              .aspectRatio(1f)
-                              .clip(RoundedCornerShape(5.dp))
-                              .background(Color.Green),
-                          contentAlignment = Alignment.Center
-                      ) {
-                          Text(text = "Item $i")
-                      }
-
-                  }
+            items(data){ item ->
+                CellItem(item = item)
+            }
         },
     )
 
-}
-
-fun onCellClick2(state: MutableState<Boolean>) {
-    Log.w("elox","State0: ${state.value}")
-    state.value = !state.value
-    Log.w("elox","State1: ${state.value}")
 }
