33f58209dbcffb869a539fa450d232ca275ceed2
MatLeg <mat.legierskii@gmail.com>
2023-08-21 18:14:19 +0200

    LazyVerticalGrid init implementation

--- app/src/main/java/com/example/connectcoins/MainActivity.kt
+++ app/src/main/java/com/example/connectcoins/MainActivity.kt
@@ -6,14 +6,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -22,12 +29,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.connectcoins.ui.theme.ConnectCoinsTheme
 import kotlin.random.Random
+
+//todo move to cofnig class
+val SIZE = 3
+val RANGE = 0..SIZE
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,18 +54,44 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 
-                    val SIZE = 5
-                    val data = (1..SIZE).map { item ->
-                        Cell(item, "Cell$item", mutableStateOf(false))
-                    }
-                    val dataHeader = (1..SIZE).map { item ->
-                        Cell(item, "Column $item", mutableStateOf(false))
+
+//                    val data = RANGE.map { item ->
+//                        Cell(item, "Cell$item", mutableStateOf(false))
+//                    }
+                    val dataHeader = RANGE.map { item ->
+                        Cell(item, "Column $item", false)
                     }
 
+//todo bulid array
+                    val row: Array<Cell> = RANGE.map {
+                           Cell(it, "Column $it", false)
+                        }.toTypedArray()
+
+                    val arrayData : Array<Array<Cell>> = RANGE.map {
+                        row
+                    }.toTypedArray()
+
+
+                    var c = 0
+                    for (x in RANGE) {
+                        for (y in RANGE) {
+                            Log.w("elox",">>>[${x}[${y}]=$c")
+                            arrayData[x][y] = Cell(c++, "Cell $c", false)
+                        }
+                    }
+
+                    var display = ""
+                    for (x in RANGE) {
+                        for (y in RANGE) {
+                            display+="${arrayData[x][y].index} |"
+                        }
+                        display+="\n"
+                    }
+                    Log.e("elox","DISPLAY: \n $display")
 
                     val x = Random.nextBoolean()
 
-                    TableScreen(SIZE, data, dataHeader, x)
+                    TableScreen(SIZE, arrayData, dataHeader, x)
                 }
             }
         }
@@ -74,7 +114,7 @@ fun RowScope.TableCell(
         onClick = onClick
     ) {
         Text(
-            text = selected.toString(),
+            text = text.toString(),
         )
     }
 }
@@ -82,12 +122,13 @@ fun RowScope.TableCell(
 class Cell(
     val index: Int,
     val text: String,
-    var state: MutableState<Boolean>
+    //var state: MutableState<Boolean>
+    var state: Boolean
 ) {
     fun changeState() {
         Log.w("elox","Cell clicked! Text=$text, Index: $index")
         Log.w("elox","State0: $state")
-        state.value = !state.value
+        state = !state
         Log.w("elox","State1: $state")
 
     }
@@ -97,11 +138,12 @@ class Cell(
 @Composable
 fun TableScreen(
     SIZE: Int = 5,
-    data: List<Cell> = (1..SIZE).map { item ->
-        Cell(item, "Cell$item", mutableStateOf(false))
-    },
-    dataHeader: List<Cell> = (1..SIZE).map { item ->
-        Cell(item, "Cell$item", mutableStateOf(false))
+    data: Array<Array<Cell>> = arrayOf(
+        arrayOf(Cell(1, "Cell", false)),
+        arrayOf(Cell(1, "Cell", false)),
+    ),
+    dataHeader: List<Cell> = RANGE.map { item ->
+        Cell(item, "Cell$item", false)
     },
     state: Boolean = false
 ) {
@@ -112,31 +154,29 @@ fun TableScreen(
     // Each cell of a column must have the same weight.
     val column1Weight = .3f // 30%
     val column2Weight = .7f // 70%
-    // The LazyColumn will be our table. Notice the use of the weights below
-    LazyColumn(
-        Modifier
-            .fillMaxSize()
-            .fillMaxHeight()
-            .padding(8.dp)) {
-        // Here is the header
-        item {
-            Row(Modifier.background(Color.Gray)) {
-                TableCell(text = dataHeader[0].text, weight = column2Weight, x.value) { onCellClick2(x) }
-                TableCell(text = dataHeader[1].text, weight = column2Weight, x.value) { onCellClick2(x) }
-                TableCell(text = dataHeader[2].text, weight = column2Weight, x.value) { onCellClick2(x) }
-                TableCell(text = dataHeader[3].text, weight = column2Weight, x.value) { onCellClick2(x) }
-            }
-        }
-        // Here are all the lines of your table.
-        items(data) {
-            Row(Modifier.fillMaxWidth()) {
-                TableCell(text = it.text, weight = column2Weight, it.state.value) { it.changeState() }
-                TableCell(text = it.text, weight = column2Weight, it.state.value) { it.changeState() }
-                TableCell(text = it.text, weight = column2Weight, it.state.value) { it.changeState() }
-                TableCell(text = it.text, weight = column2Weight, it.state.value) { it.changeState() }
-            }
-        }
-    }
+    val state = rememberLazyGridState(
+        initialFirstVisibleItemIndex = 99
+    )
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(SIZE), //Adaptive 100dp
+        state = state,
+        content = {
+                  items(100){ i ->
+                      Box(
+                          modifier = Modifier
+                              .padding(8.dp)
+                              .aspectRatio(1f)
+                              .clip(RoundedCornerShape(5.dp))
+                              .background(Color.Green),
+                          contentAlignment = Alignment.Center
+                      ) {
+                          Text(text = "Item $i")
+                      }
+
+                  }
+        },
+    )
+
 }
 
 fun onCellClick2(state: MutableState<Boolean>) {
