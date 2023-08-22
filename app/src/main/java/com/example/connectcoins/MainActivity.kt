0e5cb60882eb171c0c1e3f09302ddd82140d5e82
MatLeg <mat.legierskii@gmail.com>
2023-08-22 20:04:06 +0200

    bottom sheet modal added, dependency updated

--- app/src/main/java/com/example/connectcoins/MainActivity.kt
+++ app/src/main/java/com/example/connectcoins/MainActivity.kt
@@ -10,17 +10,32 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -69,10 +84,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
+            var showSheet by remember { mutableStateOf(false) }
+
+            if (showSheet) {
+                BottomSheet() {
+                    showSheet = false
+                }
+            }
+
             ConnectCoinsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    //modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
 
@@ -80,7 +103,19 @@ class MainActivity : ComponentActivity() {
                         Cell(item, "Column $item", mutableStateOf(false))
                     }
 
-                    TableScreen(SIZE, DATA0, dataHeader)
+                    Column() {
+
+                        TableScreen(SIZE, DATA0, dataHeader)
+
+                        Button(
+                            modifier = Modifier
+                                .height(50.dp),
+                            onClick = {
+                                showSheet = true
+                            }) {
+                            Text(text = "Show BottomSheet")
+                        }
+                    }
                 }
             }
         }
@@ -144,6 +179,28 @@ fun SingleColumn(items: Array<Cell>, columnIdx: Int) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheet(onDismiss: () -> Unit) {
+    val modalBottomSheetState = rememberModalBottomSheetState()
+
+    ModalBottomSheet(
+        onDismissRequest = { onDismiss() },
+        sheetState = modalBottomSheetState,
+        dragHandle = { BottomSheetDefaults.DragHandle() },
+    ) {
+        Text(
+            text = "Bottom sheet",
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(
+            text = "Click outside the bottom sheet to hide it",
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
 fun onCellClick(index: Int, column: Int) {
     Log.w("elox","Index: $index, column: $column")
     val cell = DATA0[column].findLast { !it.state.value }
@@ -163,119 +220,112 @@ fun displayState() {
         display += "\n"
     }
     Log.e("elox", display)
-    checkWin()
+
+    val isWinner = checkWin()
+    if (isWinner) {
+
+    }
 }
 
 val WIN = 3
-fun checkWin() {
-   // checkVertical()
-   // checkHorizontal()
-//    checkDiagonal2b()
-//    Log.e("elox","~~~~~~~~~~")
-//    checkDiagonal2FromRIght()
-//    Log.e("elox","~~~~~~~~~~")
-    checkDiagonalLeftTop()
 
+fun checkWin(): Boolean {
+    return if (checkHorizontal()) {
+        Log.w("elox",">>>WIN by checkHorizontal")
+        true
+    }
+        else if (checkVertical()) {
+        Log.w("elox",">>>WIN by checkVertical")
+            true
+    }
+    else false
+    //todo update
+
+//        else if (checkDiagonalLeftBottomAndRightTop()) {
+//        Log.w("elox",">>>WIN by checkDiagonalLeftBottomAndRightTop")
+//            true
+//    }
+//        else if (checkDiagonalRightBottom()) {
+//        Log.w("elox",">>>WIN by checkDiagonalRightBottom")
+//            true
+//    }
+//        else {
+//        Log.w("elox",">>>WIN by checkDiagonalLeftTop")
+//            checkDiagonalLeftTop()
+//    }
 }
 
 
-fun checkVertical() {
+fun checkHorizontal(): Boolean {
     var result = 0
     for (x in DATA0.indices) {
         for (y in DATA0[0].indices) {
             val cell = DATA0[y][x]
             result = if (cell.state.value) result + 1 else 0
-            if (result == WIN) Log.e("elox", ">>>WIN horizontal<<<")
+            if (result == WIN) return true
         }
         result = 0
     }
+    return false
 }
 
-fun checkHorizontal() {
+fun checkVertical(): Boolean {
     var result = 0
     for (x in DATA0.indices) {
         for (y in DATA0[0].indices) {
             val cell = DATA0[x][y]
             result = if (cell.state.value) result + 1 else 0
-            if (result == WIN) Log.e("elox", ">>>WIN vertical <<<")
+            if (result == WIN) return true
         }
         result = 0
     }
+    return false
 }
 
-fun checkDiagonal() {
-    var result = 0
-    for (x in DATA0.indices) {
-        for (y in DATA0[0].indices) {
-            if (x==y) {
-                val cell = DATA0[x][y]
-                result = if (cell.state.value) result + 1 else 0
-            }
-            if (result == WIN) Log.e("elox", ">>>WIN DIAGONAL <<<")
-        }
-    }
-}
-
-fun checkDiagonal2() {
-    var result = 0
-    val ES = 8
-    for (x in 0 .. ES) {
-        for (y in 0 .. ES - x) {
-            val point = Pair(x+y,y)
-          //  val cell = DATA0[point.first][point.second]
-            Log.e("elox","Checked point: ${point.first},${point.second}")
-       //     result = if (cell.state.value) result + 1 else 0
-            if (result == WIN) Log.e("elox", ">>>WIN DIAGONAL <<<")
-        }
-        Log.e("elox","===========================")
-    }
-}
-
-fun checkDiagonalLBandRB() {
-    var result = 0
-    val ES = 8
-    for (x in 0 .. ES) {
-        for (y in 0 .. ES - x) {
+fun checkDiagonalLeftBottomAndRightTop():Boolean {
+    var result1 = 0
+    var result2 = 0
+    for (x in 0 .. SIZE) {
+        for (y in 0 .. SIZE - x) {
             val pointLB = Pair(x+y,y)
             val pointRT = Pair(y,x+y)
-            //  val cell = DATA0[point.first][point.second]
-//            Log.e("elox","Checked point: ${pointLB.first},${pointLB.second}")
-//            Log.w("elox","Checked point: ${pointRT.first},${pointRT.second}")
-            //     result = if (cell.state.value) result + 1 else 0
-            if (result == WIN) Log.e("elox", ">>>WIN DIAGONAL <<<")
+              val cell1 = DATA0[pointLB.first][pointLB.second]
+            result1 = if (cell1.state.value) result1 + 1 else 0
+            if (result1 == WIN) return true
+
+            val cell2 = DATA0[pointRT.first][pointRT.second]
+            result2 = if (cell2.state.value) result2 + 1 else 0
+            if (result2 == WIN) return true
         }
-        Log.e("elox","===========================")
     }
+    return false
 }
 
 
-fun checkDiagonalRightBottom() {
+fun checkDiagonalRightBottom(): Boolean {
     var result = 0
-    val ES = 8
-    for (x in 0 until ES+1) {
-        for (y in 0 until ES-x+1) {
-//            val pointRB = Pair(x+y,ES-y)
-            val pointLT = Pair(ES-x,x+y)
-            //  val cell = DATA0[point.first][point.second]
-            Log.e("elox","Checked point: ${pointLT.first},${pointLT.second}")
-//            Log.w("elox","Checked point: ${pointRB.first},${pointRB.second}")
-            //     result = if (cell.state.value) result + 1 else 0
-            if (result == WIN) Log.e("elox", ">>>WIN DIAGONAL <<<")
+    for (x in 0 until SIZE+1) {
+        for (y in 0 until SIZE-x+1) {
+            val pointRB = Pair(x+y,SIZE-y)
+            val cell = DATA0[pointRB.first][pointRB.second]
+             result = if (cell.state.value) result + 1 else 0
+            if (result == WIN) return true
         }
-        Log.e("elox","===========================")
     }
+    return false
 }
 
-fun checkDiagonalLeftTop() {
-    val ES = 8
-    for (x in 0 until ES+1) {
-        for (y in x until ES+1) {
-            val pointLT = Pair(ES-y,y-x)
-            Log.e("elox","Checked point: ${pointLT.first},${pointLT.second}")
-
+fun checkDiagonalLeftTop(): Boolean {
+    var result = 0
+    for (x in 0 until SIZE+1) {
+        for (y in x until SIZE+1) {
+            val pointLT = Pair(SIZE-y,y-x)
+            val cell = DATA0[pointLT.first][pointLT.second]
+            result = if (cell.state.value) result + 1 else 0
+            if (result == WIN) return true
         }
-        Log.e("elox","===========================")
     }
+    return false
 }
 
 
