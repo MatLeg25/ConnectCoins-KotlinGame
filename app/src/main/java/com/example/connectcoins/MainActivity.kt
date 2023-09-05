6bcd639cf0fe274451a01a2803aa43c926f60e54
MatLeg <mat.legierskii@gmail.com>
2023-09-05 21:41:02 +0200

    top bar added

--- app/src/main/java/com/example/connectcoins/MainActivity.kt
+++ app/src/main/java/com/example/connectcoins/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.connectcoins
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SheetValue
@@ -21,22 +29,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.connectcoins.data.Cell
-import com.example.connectcoins.data.DATA
 import com.example.connectcoins.data.Player
 import com.example.connectcoins.ui.BottomModal
 import com.example.connectcoins.ui.GameUiState
 import com.example.connectcoins.ui.GameViewModel
-import com.example.connectcoins.ui.setSheetState
 import com.example.connectcoins.ui.theme.ConnectCoinsTheme
 
 
@@ -62,6 +74,8 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.fillMaxWidth(),
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
+
+                        TopBar()
 
                         TableScreen(
                             gameUiState,
@@ -96,14 +110,75 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 500)
+@Composable
+fun TopBar() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Magenta)
+            .padding(horizontal = 20.dp)
+
+    ) {
+        Row(
+            modifier = Modifier.padding(10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ColourText(
+                text = stringResource(id = R.string.app_name)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Card(
+                modifier = Modifier
+                    .size(48.dp)
+                    .clickable {
+                               Log.w("elox","Open settings")
+                    },
+                shape = CircleShape,
+            ) {
+                Image(
+                    painterResource(R.drawable.ic_settings),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ColourText(text: String) {
+    val gradientColors = listOf(Color.Cyan, Color.Blue, Color.Green, Color.Yellow /*...*/)
+
+    Text(
+        text = buildAnnotatedString {
+            withStyle(
+                SpanStyle(
+                    brush = Brush.linearGradient(
+                        colors = gradientColors
+                    ),
+                    fontWeight = FontWeight.ExtraBold
+                )
+            ) {
+                append(text)
+            }
+        }
+    )
+
+}
+
+@Preview(showBackground = true, widthDp = 500)
 @Composable
 fun TableScreen(
     gameUiState: GameUiState = GameUiState(Player("a",Color.Red)),
     viewModel: GameViewModel = viewModel(),
     ) {
 
-    Row() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
         viewModel.gameboard.forEachIndexed { index, column ->
             SingleColumn(
                 items = column,
