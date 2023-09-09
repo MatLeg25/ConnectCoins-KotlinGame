d4062d896795905b952627737d1b233760b06c62
MatLeg <mat.legierskii@gmail.com>
2023-09-09 20:28:42 +0200

    splash screen init implementation

--- app/src/main/java/com/example/connectcoins/MainActivity.kt
+++ app/src/main/java/com/example/connectcoins/MainActivity.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.connectcoins.data.Cell
@@ -55,6 +56,7 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
 
             ConnectCoinsTheme {
