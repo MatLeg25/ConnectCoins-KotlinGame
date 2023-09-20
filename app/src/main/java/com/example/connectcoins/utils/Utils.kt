e19ec17a43906dc5ba0e52f42e3cde47c43ad69f
MatLeg <mat.legierskii@gmail.com>
2023-09-20 21:21:54 +0200

    refactor, get init random background implemented

--- app/src/main/java/com/example/connectcoins/utils/Utils.kt
+++ app/src/main/java/com/example/connectcoins/utils/Utils.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.unit.dp
 import com.example.connectcoins.R
 import com.example.connectcoins.ui.gameBoardHorizontalPadding
 import com.example.connectcoins.ui.gameBoardInnerColumnPadding
+import kotlin.random.Random
 
 object Utils {
 
@@ -21,19 +22,15 @@ object Utils {
         R.drawable.wallhaven_j5px5q,
         R.drawable.wallhaven_4ojpwl,
         R.drawable.wallhaven_nzp29v,
-
         R.drawable.wallhaven_qdjrl7,
         R.drawable.wallhaven_w8ejlx,
-
         R.drawable.wallhaven_4gjd8e,
         R.drawable.wallhaven_4xplev,
         R.drawable.wallhaven_n6q7v7,
-
         R.drawable.wallhaven_4ddrwj,
         R.drawable.wallhaven_4l5wxy,
         R.drawable.wallhaven_4xepql,
         R.drawable.wallhaven_ym731x,
-
         R.drawable.wallhaven_2kp1wg,
         R.drawable.wallhaven_42d8dy,
         R.drawable.wallhaven_45gvl5,
@@ -41,18 +38,15 @@ object Utils {
         R.drawable.wallhaven_4gvv9q,
         R.drawable.wallhaven_6oo7mx,
         R.drawable.wallhaven_76le39,
-
         R.drawable.wallhaven_48572j,
         R.drawable.wallhaven_nmx3w1,
         R.drawable.wallhaven_nz5o7y,
         R.drawable.wallhaven_nze32y,
         R.drawable.wallhaven_qddqel,
         R.drawable.wallhaven_x1jgxv,
-
-
     )
 
-    private var backgroundIndex: Int = 1
+    private var backgroundIndex: Int = BACKGROUNDS.indices.random()
 
     private fun getMinScreenDimension(configuration: Configuration): Dp {
         val screenHeight = configuration.screenHeightDp.dp
@@ -70,7 +64,6 @@ object Utils {
     fun getNextBackground(): Int {
         val nextBackgroundIndex = backgroundIndex+1
         backgroundIndex = nextBackgroundIndex.takeIf { it in BACKGROUNDS.indices } ?: 0
-        Log.e("elox","EEEEEEEEEE: $backgroundIndex")
         return BACKGROUNDS[backgroundIndex]
     }
 }
